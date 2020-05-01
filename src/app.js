9e76e8359e17fc0b04aec24bd2854f91904a2f55
ahmedgalaldev <ahmedgalaldev@gmail.com>
2020-05-01 14:36:21 +0200

    dynamic template using hbs

--- src/app.js
+++ src/app.js
@@ -2,12 +2,28 @@ const path=require('path')
 const express = require('express')
 
 const app = express()
+app.set('view engine', 'hbs')
 
 const publicDirectoryPath=path.join(__dirname,'../public')
 app.use(express.static(publicDirectoryPath))
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:"home page",
+        name:"Ahmed Galal"
+    })
+})
 
-
-
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:"About",
+        name:"Ahmed Galal"
+    })
+})
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        msg:"Can We Help You"
+    })
+})
 app.get("/weather",(req,res)=>{
     res.send(
         {
