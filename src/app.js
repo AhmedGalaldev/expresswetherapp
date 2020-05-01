90910862c599e456b90f26616482f2662bcc1a4d
ahmedgalaldev <ahmedgalaldev@gmail.com>
2020-05-01 03:22:42 +0200

    render html and json

--- src/app.js
+++ src/app.js
@@ -3,18 +3,30 @@ const express = require('express')
 const app = express()
 
 app.get('',(req,res)=>{
-    res.send('hello express')
+    res.send('<h1>hello express</h1>')
 })
 
 app.get('/help',(req,res)=>{
-    res.send("help page")
+    res.send([
+        {
+            name:"Ahmed"
+        },
+        {
+            name:"Ali"
+        }
+    ])
 })
 
 app.get('/about',(req,res)=>{
-    res.send("about page")
+    res.send("<h3>about page</h3>")
 })
 app.get("/weather",(req,res)=>{
-    res.send("weahter page")
+    res.send(
+        {
+            forcecast:"it is hot",
+            location:"Egypt"
+        }
+    )
 })
 app.listen(3000,()=>{
     console.log("Server is up on port 3000.");
