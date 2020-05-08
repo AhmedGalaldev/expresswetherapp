0f3db87b76d0b856b06e25e8c8c088e049add3cd
ahmedgalaldev <ahmedgalaldev@gmail.com>
2020-05-08 02:17:15 +0200

    use utils

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path=require('path')
 const express = require('express')
 const hbs = require('hbs')
+const foreCast = require('./utils/forecast')
+const geoCode = require('./utils/geocode')
 
 const app = express()
 // express config 
@@ -40,12 +42,45 @@ app.get('/help',(req,res)=>{
     })
 })
 app.get("/weather",(req,res)=>{
-    res.send(
-        {
-            forcecast:"it is hot",
-            location:"Egypt"
+    if(!req.query.address){
+        return res.send({
+            error:"no address"
+        })
+    }
+    geoCode(req.query.address,(error,{location,longitude,lititude})=>{
+        if(error){
+            return res.send({error})
         }
-    )
+        foreCast(longitude,lititude,(error,forCasteData)=>{
+            if(error){
+                return res.send({error})
+            }
+            res.send({
+                forecast:forCasteData,
+                location,
+                address:req.query
+            })
+        })
+    })
+
+    // res.send(
+    //     {
+    //         forcecast:"it is hot",
+    //         location:"Egypt",
+    //         address:req.query.address
+    //     }
+    // )
+})
+
+app.get("/products",(req,res)=>{
+    if(!req.query.search){
+        return res.send({
+            error:"there is no search in request"
+        })
+    }
+    res.send({
+        "products":[]
+    })
 })
 app.get('/help/*',(req,res)=>{
     res.render('error',{
